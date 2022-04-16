723ad26aeb9e1b7b78ad37332ca8caea1bbebdc5
Devon Peticolas <devon@oden.io>
2022-04-16 18:15:55 -0400

    Add edit and event links to the Admin RSVP list view

--- events/admin.py
+++ events/admin.py
@@ -4,8 +4,6 @@ from django.utils.html import format_html
 
 from .models import RSVP, Event
 
-admin.site.register(RSVP)
-
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
@@ -23,4 +21,28 @@ class EventAdmin(admin.ModelAdmin):
     def edit_link(self, event):
         """Link to the event detail page."""
         url = reverse("events:update", kwargs={"pk": event.id}) + "?" + "secret=" + event.secret()
+        return format_html('<a href="{}">Edit</a>', url)
+
+
+@admin.register(RSVP)
+class RSVPAdmin(admin.ModelAdmin):
+    list_display = ["name", "event_link", "edit_link"]
+
+    class Meta:
+        model = RSVP
+        fields = "__all__"
+
+    def event_link(self, rsvp):
+        """Link to the event detail page."""
+        url = reverse("events:detail", kwargs={"pk": rsvp.event.id})
+        return format_html('<a href="{}">{}</a>', url, rsvp.event.title)
+
+    def edit_link(self, rsvp):
+        """Link to the rsvp detail page."""
+        url = (
+            reverse("events:rsvp_update", kwargs={"event_id": rsvp.event.id, "pk": rsvp.id})
+            + "?"
+            + "secret="
+            + rsvp.secret()
+        )
         return format_html('<a href="{}">Edit</a>', url)
